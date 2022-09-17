2adde5a55006fb123b7a1f1d520bb3596e24f2fd
Danilo Pereira <danilopx1@gmail.com>
2022-09-17 11:13:41 -0300

    New: Post Ad

---
+++ web-esports/src/components/CreateAdBanner.tsx
@@ -0,0 +1,21 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { MagnifyingGlassPlus } from 'phosphor-react'
+export default function CreateAdBanner(){
+  return (
+    <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8 '>
+      <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center"> 
+        <div>
+          <strong className='text-2xl block font-black text-white'>Não encontrou seu duo? </strong>
+          <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
+        </div>
+        <Dialog.Trigger 
+          className='py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-600 flex items-center gap-3' 
+          onClick={()=>{}}
+          >
+            <MagnifyingGlassPlus /> 
+            Publicar anúncio
+        </Dialog.Trigger>
+      </div>
+    </div>
+  )
+}
