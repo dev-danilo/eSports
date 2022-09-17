0694f5fcecc802d554fba108c31485d2df341d79
Danilo Pereira <danilopx1@gmail.com>
2022-09-17 14:34:19 -0300

    New: List Games

--- mobile-esports/App.tsx
+++ mobile-esports/App.tsx
@@ -2,7 +2,7 @@ import { Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black, use
 import { StatusBar } from 'react-native';
 import { Background } from './src/components/Background';
 import { Loading } from './src/components/Loading';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -15,7 +15,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      {fontsLoaded ? <Home />: <Loading/>}
+      {fontsLoaded ? <Routes />: <Loading/>}
     </Background>
   );
 }
---
+++ mobile-esports/src/routes/app.routes.tsx
@@ -0,0 +1,27 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Game } from '../screens/Game'
+import { Home } from '../screens/Home'
+
+
+const {Navigator, Screen} = createNativeStackNavigator()
+
+export function AppRoutes() {
+  return (
+    <Navigator /* screenOptions-{{headerShown: false}}*/>
+      <Screen
+        name="home"
+        component={Home}
+        options={{
+          headerShown: false
+        }}
+      />
+      <Screen
+        name="game"
+        component={Game}
+        options={{
+          headerShown: false
+        }}
+      />
+    </Navigator>
+  )
+}
