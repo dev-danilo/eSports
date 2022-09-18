eeca0d035454102873330a0e5f969888f34db3aa
Danilo Pereira <danilopx1@gmail.com>
2022-09-18 08:55:34 -0300

    New: Mobile finish

--- mobile-esports/src/components/DuoCard/index.tsx
+++ mobile-esports/src/components/DuoCard/index.tsx
@@ -20,7 +20,7 @@ interface Props{
   onConnect: () => void;
 }
 
-export function DuoCard({data, onConnect}: Props) {
+export function DuoCard({ data, onConnect }: Props) {
   return (
     <View style={styles.container}>
       <DuoInfo
